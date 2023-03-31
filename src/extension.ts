49cc859c4efa34fa30eab776270db4f8e76aff4f
yunier-olivera <yoliveraespinosa@gmail.com>
2023-03-31 16:05:33 -0700

    Code formatting

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,4 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // [ ] Show extension on status bar
@@ -15,13 +14,6 @@ import * as vscode from 'vscode';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-
-  // This line of code will only be executed once when your extension is activated
-  // console.log(
-  //   'Congratulations, your extension "markdown-quarto-word-count" is now active!'
-  // );
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
