10eb3f56ae8f594436b71b3b303f0a71a9bcd34b
yunier-olivera <yoliveraespinosa@gmail.com>
2023-04-01 18:26:26 -0700

    Code optimized and working

--- src/extension.ts
+++ src/extension.ts
@@ -1,180 +1,61 @@
-// The module 'vscode' contains the VS Code extensibility API
 import * as vscode from 'vscode';
 
-// [x] Show extension on status bar
-// [x] The status bar should be up date with the text
-// [ ] Optimize code
-// [ ] Add functionality to count words in selected text
-// [ ] Add screenshots
-// [ ] Add character count
-// [ ] Add reading time
-// [ ] Add speaking time
-// [ ] Add readability score?
-// [x] Update Change log
-// [x] Publish update, use `vsce package` then `vsce publish major|minor|patch`
+// Prepare text by removing unwanted characters:
+function prepareText(text: string): string {
+  // remove YAML front matter
+  text = text.replace(/^---[\s\S]+?---/, '');
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+  // remove code blocks
+  text = text.replace(/```[\s\S]+?```/g, '');
+
+  // remove inline code
+  text = text.replace(/`[^`]+?`/g, '');
+
+  // remove HTML tags
+  text = text.replace(/<[^>]+?>/g, '');
+
+  // remove punctuation and special characters
+  text = text.replace(/[^\w\s]|_/g, '');
+
+  // remove multiple whitespaces and line breaks
+  text = text.replace(/\s+/g, ' ').trim();
+
+  return text;
+}
+
+let wordCountStatusBarItem: vscode.StatusBarItem;
+
 export function activate(context: vscode.ExtensionContext) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'markdown-quarto-word-count.countWords',
     () => {
-      // Create a status bar item
-      const statusBarItem = vscode.window.createStatusBarItem(
-        vscode.StatusBarAlignment.Left,
-        100
-      );
-
-      // The code you place here will be executed every time your command is executed
       const editor = vscode.window.activeTextEditor;
-      if (editor) {
-        let text = editor.document.getText();
-        // Prepare text by removing unwanted characters:
+      try {
+        const text = editor?.document.getText() ?? '';
+        const preparedText = prepareText(text);
+        const wordCount = preparedText.split(/\s+/).length;
 
-        // remove all line breaks
-        text = text.replace(/[\r\n]/g, ' ');
-
-        // do not include yaml front matter
-        const threeDashes = text.match(/---/g);
-        if (
-          threeDashes &&
-          threeDashes.length >= 2 &&
-          threeDashes[0] === '---'
-        ) {
-          const yamlEnd = text.indexOf('---', 3) + 3;
-          text = text.substring(yamlEnd + 1);
-        }
-
-        // do not include text in code chunks
-        text = text.replace(/```{.+?}[\s\S]+?```/g, '');
-
-        // do not include text in in-line R code
-        text = text.replace(/`r.+?`/g, '');
-
-        // do not include HTML comments
-        text = text.replace(/<!--[\s\S]+?-->/g, '');
-
-        // do not include images with captions
-        text = text.replace(/!\[.+?\)/g, '');
-
-        // do not include inline markdown URLs
-        text = text.replace(/\(http.+?\)/g, '');
-
-        // do not include # for headings
-        text = text.replace(/#+/g, '');
-
-        // do not include opening HTML tags
-        const htmlTags = require('./htmlTags.json').htmlTags;
-        const htmlTagPattern = new RegExp(
-          `<(${htmlTags.join('|')})[^>]*>`,
-          'g'
-        );
-        text = text.replace(htmlTagPattern, '');
-
-        // do not include closing html tags
-        text = text.replace(/<\/.+?>/g, '');
-
-        // do not include greater/less than signs
-        text = text.replace(/[<>]/g, '');
-
-        // do not include percent signs because they trip up stringi
-        text = text.replace(/%/g, '');
-
-        // do not include figures and tables inserted using plain LaTeX code
-        text = text.replace(/\\begin{figure\}[\s\S]*?\\end{figure}/g, '');
-        text = text.replace(/\\begin{table\}[\s\S]*?\\end{table}/g, '');
-
-        // do not count abbreviations as multiple words, but leave
-        // the period at the end in case it's the end of a sentence
-        text = text.replace(/\.(?=[a-z]+)/g, '');
-        if (text.length === 0) {
-          throw new Error(
-            'You have not selected any text. Please select some text with the mouse and try again'
-          );
-        }
-
-        const wordCount = text.split(/\s+/).length;
-
-        // vscode.window.showInformationMessage(`Word count: ${wordCount}`);
-        statusBarItem.text = `Word count: ${wordCount}`;
-        statusBarItem.show();
+        wordCountStatusBarItem.text = `Word count: ${wordCount}`;
+        wordCountStatusBarItem.show();
+      } catch (error: any) {
+        vscode.window.showErrorMessage(error.message);
       }
     }
   );
 
   context.subscriptions.push(disposable);
 
-  // Create a status bar item
-  const wordCountStatusBarItem = vscode.window.createStatusBarItem(
+  wordCountStatusBarItem = vscode.window.createStatusBarItem(
     vscode.StatusBarAlignment.Left,
     100
   );
 
   // Update the word count in the status bar
   function updateWordCountStatusBarItem(editor: vscode.TextEditor) {
-    let text = editor.document.getText();
+    const text = editor?.document.getText() ?? '';
+    const preparedText = prepareText(text);
+    const wordCount = preparedText.split(/\s+/).length;
 
-    // Prepare text by removing unwanted characters:
-
-    // remove all line breaks
-    text = text.replace(/[\r\n]/g, ' ');
-
-    // do not include yaml front matter
-    const threeDashes = text.match(/---/g);
-    if (threeDashes && threeDashes.length >= 2 && threeDashes[0] === '---') {
-      const yamlEnd = text.indexOf('---', 3) + 3;
-      text = text.substring(yamlEnd + 1);
-    }
-
-    // do not include text in code chunks
-    text = text.replace(/```{.+?}[\s\S]+?```/g, '');
-
-    // do not include text in in-line R code
-    text = text.replace(/`r.+?`/g, '');
-
-    // do not include HTML comments
-    text = text.replace(/<!--[\s\S]+?-->/g, '');
-
-    // do not include images with captions
-    text = text.replace(/!\[.+?\)/g, '');
-
-    // do not include inline markdown URLs
-    text = text.replace(/\(http.+?\)/g, '');
-
-    // do not include # for headings
-    text = text.replace(/#+/g, '');
-
-    // do not include opening HTML tags
-    const htmlTags = require('./htmlTags.json').htmlTags;
-    const htmlTagPattern = new RegExp(`<(${htmlTags.join('|')})[^>]*>`, 'g');
-    text = text.replace(htmlTagPattern, '');
-
-    // do not include closing html tags
-    text = text.replace(/<\/.+?>/g, '');
-
-    // do not include greater/less than signs
-    text = text.replace(/[<>]/g, '');
-
-    // do not include percent signs because they trip up stringi
-    text = text.replace(/%/g, '');
-
-    // do not include figures and tables inserted using plain LaTeX code
-    text = text.replace(/\\begin{figure\}[\s\S]*?\\end{figure}/g, '');
-    text = text.replace(/\\begin{table\}[\s\S]*?\\end{table}/g, '');
-
-    // do not count abbreviations as multiple words, but leave
-    // the period at the end in case it's the end of a sentence
-    text = text.replace(/\.(?=[a-z]+)/g, '');
-    if (text.length === 0) {
-      throw new Error(
-        'You have not selected any text. Please select some text with the mouse and try again'
-      );
-    }
-
-    const wordCount = text.split(/\s+/).length;
     wordCountStatusBarItem.text = `Word count: ${wordCount}`;
     wordCountStatusBarItem.show();
   }
@@ -199,3 +80,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+// [x] Show extension on status bar
+// [x] The status bar should be up date with the text
+// [ ] Optimize code
+// [ ] Add functionality to count words in selected text
+// [ ] Add screenshots
+// [ ] Add character count
+// [ ] Add reading time
+// [ ] Add speaking time
+// [ ] Add readability score?
+// [x] Update Change log
+// [x] Publish update, use `vsce package` then `vsce publish major|minor|patch`
